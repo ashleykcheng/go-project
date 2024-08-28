cf5b3b5b47c7ad1fadefd16ccd6f54cdcdaa5772
ashleykcheng <ashleykcheng@gmail.com>
2024-08-27 19:27:12 -0700

    mini game

--- tutorial.go
+++ tutorial.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Welcome to my quiz game!")
+	fmt.Printf("Please input your name: ")
+	var name string
+	fmt.Scan(&name)
+	fmt.Printf("Hello, %v, welcome to the game!", name)
+
+	fmt.Printf("Please enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+	if age >= 10 {
+		fmt.Println("You can play!")
+	} else {
+		fmt.Println("You cannot play")
+		return
+	}
+
 }
